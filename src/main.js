418529ad10eebcaf87ff64a773825fedf3cf7a3c
JoaoDums <joaopedro23342345555@gmail.com>
2026-02-09 18:52:18 -0300

    criação do router

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ToastPlugin from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
+import router from './router'
 
 const app = createApp(App)
 
 app.use(ToastPlugin)
+app.use(router)
 
 app.mount('#app')
