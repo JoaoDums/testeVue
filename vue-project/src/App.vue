5a825e71fd7003de06fa2a62185617035860f76f
JoaoDums <joaopedro23342345555@gmail.com>
2026-02-09 17:08:35 -0300

    aprendendo a mecher no vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { useToast } from 'vue-toast-notification'
+
+const count = ref(0)
+const toast = useToast()
+
+function increment() {
+  count.value++
+  toast.success(`Valor incrementado para ${count.value}`)
+}
+
+function decrement() {
+  if (count.value === 0) {
+    toast.warning('O contador já está em 0!')
+    return
+  }
+
+  count.value--
+  toast.info(`Valor decrementado para ${count.value}`)
+}
+</script>
+
+<template>
+  <div>Count is: {{ count }}</div>
+
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
